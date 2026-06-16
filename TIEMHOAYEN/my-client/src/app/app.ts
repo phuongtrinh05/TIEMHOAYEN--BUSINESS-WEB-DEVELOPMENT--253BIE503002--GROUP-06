3f785a2c400fef2fb58e4f8c73361b769a07e826
phuongtrinh05 <trinhpnp23406@st.uel.edu.vn>
2026-06-16 16:30:43 +0700

    fix: remove unused PageHeader1, PageHeader2, PageFooter1 components

--- TIEMHOAYEN/my-client/src/app/app.ts
+++ TIEMHOAYEN/my-client/src/app/app.ts
@@ -1,8 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PageHeader } from './components/page-header/page-header';
-import { PageHeader1 } from './components/page-header-1/page-header-1';
-import { PageHeader2 } from './components/page-header-2/page-header-2';
 import { PageFooter } from './components/page-footer/page-footer';
 import { ChatbotWidget } from './components/chatbot-widget/chatbot-widget';
 
