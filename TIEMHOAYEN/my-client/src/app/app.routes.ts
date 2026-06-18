28d3a5faf263d917e18beb2e333550335d9db1c9
anhthoa795 <thoahta23406@st.uel.edu.vn>
2026-06-18 13:15:22 +0700

    Add route for Design3d component

--- TIEMHOAYEN/my-client/src/app/app.routes.ts
+++ TIEMHOAYEN/my-client/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutUs } from './pages/about-us/about-us';
 import { OrderDetail } from './pages/orders/order-detail/order-detail';
 import { OrderReview } from './pages/orders/order-review/order-review';
 import { Homepage } from './pages/homepage/homepage';
+import { Design3d } from './pages/design3d/design3d';
 export const routes: Routes = [
      {
     path: '',
@@ -33,5 +34,9 @@ export const routes: Routes = [
   {
     path: 'homepage',
     component: Homepage
-  }
+  },
+  {
+    path: 'design3d',
+    component: Design3d
+  },
 ];
