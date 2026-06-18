85c6fd48d0df021f1463b918a6a194f5c6682bda
Thuong-Thu <thuvt23406@st.uel.edu.vn>
2026-06-18 17:51:37 +0700

    Add admin sidebar and update admin styles

---
+++ TIEMHOAYEN/my-admin/src/app/components/admin-sidebar/admin-sidebar.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-sidebar',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './admin-sidebar.html',
+  styleUrl: './admin-sidebar.css'
+})
+export class AdminSidebar {
+
+  isCollapsed = false;
+
+  toggleSidebar(){
+    alert('CLICK');
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+}
