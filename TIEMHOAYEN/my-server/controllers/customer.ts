c5170c278dc7d7e9703590a724e2e9764c77ce1a
phuongtrinh05 <trinhpnp23406@st.uel.edu.vn>
2026-06-19 20:01:09 +0700

    Update customer controller and route

--- TIEMHOAYEN/my-server/controllers/customer.ts
+++ TIEMHOAYEN/my-server/controllers/customer.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { connectDB, sql } from '../db.js';
-
+const otpStore = new Map<string, string>();
 export const getAllCustomers = async (req: Request, res: Response) => {
     try {
         const pool = await connectDB();
@@ -86,4 +86,180 @@ export const registerCustomer = async (req: Request, res: Response) => {
         console.error('Lỗi registerCustomer:', error.message);
         return res.status(500).json({ message: 'Lỗi server: ' + error.message });
     }
+};
+export const loginCustomer = async (req: Request, res: Response) => {
+    try {
+        const { SDT, MAT_KHAU } = req.body;
+
+        if (!SDT || !MAT_KHAU) {
+            return res.status(400).json({
+                message: 'Vui lòng nhập đầy đủ thông tin.'
+            });
+        }
+
+        const pool = await connectDB();
+
+        const result = await pool.request()
+            .input('SDT', sql.NVarChar, SDT)
+            .input('MAT_KHAU', sql.NVarChar, MAT_KHAU)
+            .query(`
+                SELECT *
+                FROM KHACH_HANG
+                WHERE SDT = @SDT
+                AND MAT_KHAU = @MAT_KHAU
+            `);
+
+        if (result.recordset.length === 0) {
+            return res.status(401).json({
+                message: 'Sai số điện thoại hoặc mật khẩu.'
+            });
+        }
+
+        const customer = result.recordset[0];
+
+        return res.status(200).json({
+            message: 'Đăng nhập thành công.',
+            customer
+        });
+
+    } catch (error: any) {
+        return res.status(500).json({
+            message: 'Lỗi server: ' + error.message
+        });
+    }
+};
+
+export const forgotPassword = async (req: Request, res: Response) => {
+    try {
+
+        const {
+            SDT,
+            NEW_PASSWORD
+        } = req.body;
+
+        if (!SDT || !NEW_PASSWORD) {
+            return res.status(400).json({
+                message: 'Thiếu thông tin.'
+            });
+        }
+
+        const pool = await connectDB();
+
+        const checkCustomer = await pool.request()
+            .input('SDT', sql.NVarChar, SDT)
+            .query(`
+                SELECT *
+                FROM KHACH_HANG
+                WHERE SDT = @SDT
+            `);
+
+        if (checkCustomer.recordset.length === 0) {
+            return res.status(404).json({
+                message: 'Không tìm thấy tài khoản.'
+            });
+        }
+
+        await pool.request()
+            .input('SDT', sql.NVarChar, SDT)
+            .input('MAT_KHAU', sql.NVarChar, NEW_PASSWORD)
+            .query(`
+                UPDATE KHACH_HANG
+                SET MAT_KHAU = @MAT_KHAU
+                WHERE SDT = @SDT
+            `);
+
+        return res.status(200).json({
+            message: 'Đổi mật khẩu thành công.'
+        });
+
+    } catch (error: any) {
+        return res.status(500).json({
+            message: 'Lỗi server: ' + error.message
+        });
+    }
+};
+export const sendOtp = async (
+  req: Request,
+  res: Response
+) => {
+
+  try {
+
+    const { SDT } = req.body;
+
+    const pool = await connectDB();
+
+    const result = await pool.request()
+      .input('SDT', sql.NVarChar, SDT)
+      .query(`
+        SELECT *
+        FROM KHACH_HANG
+        WHERE SDT = @SDT
+      `);
+
+    if(result.recordset.length === 0){
+      return res.status(404).json({
+        message:'Số điện thoại không tồn tại'
+      });
+    }
+
+    const otp =
+      Math.floor(
+        100000 +
+        Math.random()*900000
+      ).toString();
+
+    otpStore.set(
+      SDT,
+      otp
+    );
+
+    console.log(
+      'OTP:',
+      otp
+    );
+
+    return res.status(200).json({
+      message:'Đã gửi OTP'
+    });
+
+  } catch(error:any){
+
+    return res.status(500).json({
+      message:error.message
+    });
+  }
+};
+export const verifyOtp = async (
+  req: Request,
+  res: Response
+) => {
+
+  try {
+
+    const {
+      SDT,
+      OTP
+    } = req.body;
+
+    const savedOtp =
+      otpStore.get(SDT);
+
+    if(savedOtp !== OTP){
+
+      return res.status(400).json({
+        message:'OTP không đúng'
+      });
+    }
+
+    return res.status(200).json({
+      message:'OTP hợp lệ'
+    });
+
+  } catch(error:any){
+
+    return res.status(500).json({
+      message:error.message
+    });
+  }
 };
--- TIEMHOAYEN/my-server/routes/customer.ts
+++ TIEMHOAYEN/my-server/routes/customer.ts
@@ -1,9 +1,27 @@
 import express from 'express';
-import { getAllCustomers, registerCustomer } from '../controllers/customer.js';
+
+import {
+  getAllCustomers,
+  registerCustomer,
+  loginCustomer,
+  forgotPassword,sendOtp,
+verifyOtp
+} from '../controllers/customer.js';
 
 const router = express.Router();
 
 router.get('/', getAllCustomers);
+
+// Đăng ký
 router.post('/', registerCustomer);
+
+// Đăng nhập
+router.post('/login', loginCustomer);
+
+// Quên mật khẩu
+router.put('/forgot-password', forgotPassword);
+router.post('/send-otp',sendOtp);
+
+router.post('/verify-otp',verifyOtp);
 
 export default router;
