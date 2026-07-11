3f59dc208e2d1338f651c37bc792fafd937f0f9c
Thuong_Thu <thuvt23406@st.uel.edu.vn>
2026-07-11 11:26:26 +0700

    configure admin for Vercel SPA deployment

--- TIEMHOAYEN/my-admin/src/app/app.routes.server.ts
+++ TIEMHOAYEN/my-admin/src/app/app.routes.server.ts
@@ -2,35 +2,8 @@
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'orders/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'customers/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'content/article-detail/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'orders',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'orders/order-list',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'orders/create-order',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'orders/order-detail/:id',
-    renderMode: RenderMode.Server
-  },  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
 
